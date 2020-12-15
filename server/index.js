267292b4ac69bd7b546a38afa1d192d325765e42
zhao-bin-zero <1920746358@qq.com>
2020-12-15 13:27:54 +0800

    域名测试

--- server/index.js
+++ server/index.js
@@ -5,11 +5,17 @@ app.use(cookieParser());
 
 // const { isProd } = require('./common/utils');
 // if (isProd) {
-    // 开启 gzip 压缩 https://github.com/woai3c/node-blog/blob/master/doc/optimize.md
-    const compression = require('compression');
-    app.use(compression());
+// 开启 gzip 压缩 https://github.com/woai3c/node-blog/blob/master/doc/optimize.md
+const compression = require('compression');
+app.use(compression());
 // }
 
+app.use('/:lang(UK)', (req, res) => {
+  res.redirect('https://uk.tigerwit.com/')
+});
+app.use('/:lang(hh)', (req, res) => {
+  res.send('123')
+});
 app.use(
     '/:lang(a)?',
     (req, res, next) => {
