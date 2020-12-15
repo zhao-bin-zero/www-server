30f030426eac2f1feae1bb2dd1171395db63e2cd
zhao-bin-zero <1920746358@qq.com>
2020-12-15 14:07:11 +0800

    域名测试

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,28 @@
 <template>
-  <div>
-    <h3>文章列表</h3>
-    <div class="list" v-for="i in list">
-      <router-link :to="{path:'/item/'+i.id}">{{i.title}}</router-link>
-      </div>
-  </div>
+    <div>
+        <h3>文章列表</h3>
+        <div class="list" v-for="i in list">
+            <router-link :to="{ path: '/item/' + i.id }">{{ i.title }}</router-link>
+        </div>
+    </div>
 </template>
 <script>
 export default {
-  asyncData ({store, route}){
-    return store.dispatch('fetchList')
-  },
-  computed: {
-    list () {
-      return this.$store.state.list
-    }
-  },
-  data(){
-    return {
-      name:'wfz'
-    }
-  }
-}
+    asyncData({ store, route }) {
+        // return store.dispatch('fetchList')
+    },
+    computed: {
+        list() {
+            return this.$store.state.list;
+        },
+    },
+    mounted() {
+        this.$store.dispatch('fetchList');
+    },
+    data() {
+        return {
+            name: 'wfz',
+        };
+    },
+};
 </script>
